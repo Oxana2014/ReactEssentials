85aae9127c81c7a385f17a85ed65fb11b8b25adc
Oksana Kormilina <keysb@mail.ru>
2023-12-26 13:57:13 +0400

    Added  Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import reactImage from './assets/react-core-concepts.png'
+
+const reactDescriptions = ["Fundamental", "Crisial", "Core"];
+
+function getRandomInt(max) {
+  return  Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)]
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header></Header>
       <main>
         <h2>Time to get started!</h2>
       </main>
