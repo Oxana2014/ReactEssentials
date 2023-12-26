372fe3a2c76114be2e452afe635f26133dd2a0d3
Oksana Kormilina <keysb@mail.ru>
2023-12-26 18:53:18 +0400

    Added TubButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from './components/Header/Header.jsx'
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 
 function App() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header></Header>
@@ -20,6 +24,15 @@ function App() {
              <CoreConcept {...CORE_CONCEPTS[2]} />
              <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+<h2>Examples</h2>
+<menu>
+  <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+  <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+  <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+  <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+</menu>
         </section>
       </main>
     </div>
