4745ca2a475349ce63df06c3e17a63dfef3b5461
Oksana Kormilina <keysb@mail.ru>
2023-12-27 16:26:04 +0400

    Made list of CoreConcepts updated dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -29,14 +29,15 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map(conceptItem => <CoreConcept key={conceptItem.title} { ...conceptItem} />)}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
